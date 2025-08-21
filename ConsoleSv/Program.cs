e45c0ad6c6408bfd2d89667dc7e1f4cac5e2f94d
Trung kien <trungkienyt26@gmail.com>
2025-08-21 15:14:17 +0700

    Add ConsoleSv as normal folder

---
+++ ConsoleSv/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Gameserver.core;
+
+
+var server = new ServerHostWrapper();
+await server.StartAsync();
+
+Console.WriteLine("Nhấn 'q' để thoát");
+while (Console.ReadKey(true).Key != ConsoleKey.Q) ;
+await server.StopAsync();
